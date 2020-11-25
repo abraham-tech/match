881af9b7356d0859d5d7e5a96494b1031b2752db
AbMan <abmt2God@gmail.com>
2020-11-25 11:06:16 +0100

    adding pay, feedback and status

---
+++ src/entity/Pay.java
@@ -0,0 +1,12 @@
+public class Pay {
+    private int amount;
+    private Feedback Feedback;
+
+    public Pay(int amount){
+        this.amount = amount;
+    }
+
+    public int getAmount(){
+        return amount;
+    }
+}
